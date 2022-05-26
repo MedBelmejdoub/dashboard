5bdb3e29176264a79cefb5e986f574321079e399
Med <BelmejdoubMed@gmail.com>
2022-05-26 22:01:55 +0100

    SlideBar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import react from "react";
-import { render } from "react-dom";
+import  ReactDOM  from "react-dom";
 
 import App from "./App";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
